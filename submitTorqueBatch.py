83dc33a944961a10526107c27eda609f52e6c798
Geonmo <ry840901@gmail.com>
2015-03-17 23:20:41 +0900

    Add queue option.

--- submitTorqueBatch.py
+++ submitTorqueBatch.py
@@ -19,17 +19,20 @@ class submitBatch :
     self.output_path = time_dir
 
   def getVariable( self ) :
-    parser = OptionParser()
-    parser.add_option("-i","--inpath",action="store",type="string",dest="inputPath")
-    parser.add_option("-o","--outpath",action="store",type="string",default="ANA_ROOT",dest="outputPath")
-    parser.add_option("-p","--pattern",action="store",type="string",default="*.root",dest="pattern")  
-    parser.add_option("-t","--title",action="store",type="string",default="ANA",dest="title")  
+    usage = "%prog [options] pyROOT_Script.py\negs)submitTorqueBatch.py -i /pnfs/user/cat_data/JpsiFilteredCattuple/ -p \"catTuple*.root\" calAngle.py\n"
+    parser = OptionParser(usage)
+    parser.add_option("-i","--inpath",action="store",type="string",dest="inputPath",help="input files(.ROOT)'s directory path.")
+    parser.add_option("-o","--outpath",action="store",type="string",default="ANA_ROOT",dest="outputPath",help="output file path.(Default : ANA_ROOT)")
+    parser.add_option("-p","--pattern",action="store",type="string",default="*.root",dest="pattern",help="Input file's name pattern.(Default : \"*.root\")")  
+    parser.add_option("-t","--title",action="store",type="string",default="ANA",dest="title",help="Job's Title.(Default : ANA) ")  
+    parser.add_option("-q","--queue",action="store",type="string",default="batch6",dest="queue",help="torque queue name.(Default : batch6)" )  
     (options,args) = parser.parse_args()
     #self.input_path = options.inputPath
     self.output_path = options.outputPath 
     self.title = options.title
     self.input_list = glob.glob( options.inputPath+'/'+ options.pattern)
     self.pyCfg = args[0]
+    self.queue = options.queue
   def makeCmdFiles(self, idx, input, output ) :
     script_head ="""#PBS -S /bin/bash
 #PBS -N %s_%s
@@ -63,13 +66,15 @@ eval `scramv1 runtime -sh`
       idx = re.search(r'\d+',input)
       if ( idx.group() is not None) :
         n_idx = int(idx.group())
-      output = self.output_path+"/output_%03d.root"%(int(n_idx))
+      output = self.output_path+"/"+self.title+"_%03d.root"%(int(n_idx))
       config = self.makeCmdFiles(n_idx,input,output )
       tmp_sc = open(self.output_path+"/batch_%d"%n_idx+".cmd","w")
       tmp_sc.write( config )
       tmp_sc.write("python %s %s %s"%(self.pyCfg, input, output))
       tmp_sc.close()
-      os.system("qsub -q batch6 "+self.output_path+"/batch_%d"%n_idx+".cmd")
+      command = "qsub -q %s %s/batch_%d.cmd"%(self.queue,self.output_path,n_idx)
+      print command
+      os.system(command)
     
 
 
